bf6c45c218e0d7ab521923ebd961cd67d28e76b6
Chris Patrick <atomize101@gmail.com>
2020-11-01 16:57:42 -0500

    Bringing components together, and style module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
-class App extends Component {
+class App extends React.Component {
 	render() {
-		return <div>Testing</div>;
+		return (
+			<div className={styles.container}>
+				<Cards />
+				<CountryPicker />
+				<Chart />
+			</div>
+		);
 	}
 }
 
