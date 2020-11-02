0c517691fdf3d0452d309c5b6dbe739626ee14fd
Chris Patrick <atomize101@gmail.com>
2020-11-02 11:15:32 -0500

    Turn the values into a counter format

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 
@@ -15,8 +16,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 						<Typography color="textSecondary" gutterBottom>
 							Infected
 						</Typography>
-						<Typography variant="h5">{confirmed.value}</Typography>
-						<Typography color="textSecondary">REAL DATE</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
 						<Typography variant="body2">Number of active cases of COVID-19</Typography>
 					</CardContent>
 				</Grid>
@@ -25,8 +28,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 						<Typography color="textSecondary" gutterBottom>
 							Recovered
 						</Typography>
-						<Typography variant="h5">{recovered.value}</Typography>
-						<Typography color="textSecondary">REAL DATE</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
 						<Typography variant="body2">Number of recoveries from COVID-19</Typography>
 					</CardContent>
 				</Grid>
@@ -35,8 +40,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 						<Typography color="textSecondary" gutterBottom>
 							Deaths
 						</Typography>
-						<Typography variant="h5">{deaths.value}</Typography>
-						<Typography color="textSecondary">REAL DATE</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
 						<Typography variant="body2">Number of deaths caused by COVID-19</Typography>
 					</CardContent>
 				</Grid>
