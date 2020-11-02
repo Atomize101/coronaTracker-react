5eb53cc0902f2834218e76519f73aeabfd2b9d5d
Chris Patrick <atomize101@gmail.com>
2020-11-02 10:25:12 -0500

    Displaying the data stats infected, recovered, deaths

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card, CardCotent, Typography, Grid, StylesProvider, CardContent } from '@material-ui/core';
+import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-	console.log(props);
-
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+	if (!confirmed) {
+		return 'Loading...';
+	}
 	return (
 		<div className={styles.container}>
 			<Grid container spacing={3} justify="center">
@@ -14,7 +15,7 @@ const Cards = (props) => {
 						<Typography color="textSecondary" gutterBottom>
 							Infected
 						</Typography>
-						<Typography variant="h5">REAL DATA</Typography>
+						<Typography variant="h5">{confirmed.value}</Typography>
 						<Typography color="textSecondary">REAL DATE</Typography>
 						<Typography variant="body2">Number of active cases of COVID-19</Typography>
 					</CardContent>
@@ -24,7 +25,7 @@ const Cards = (props) => {
 						<Typography color="textSecondary" gutterBottom>
 							Recovered
 						</Typography>
-						<Typography variant="h5">REAL DATA</Typography>
+						<Typography variant="h5">{recovered.value}</Typography>
 						<Typography color="textSecondary">REAL DATE</Typography>
 						<Typography variant="body2">Number of recoveries from COVID-19</Typography>
 					</CardContent>
@@ -34,7 +35,7 @@ const Cards = (props) => {
 						<Typography color="textSecondary" gutterBottom>
 							Deaths
 						</Typography>
-						<Typography variant="h5">REAL DATA</Typography>
+						<Typography variant="h5">{deaths.value}</Typography>
 						<Typography color="textSecondary">REAL DATE</Typography>
 						<Typography variant="body2">Number of deaths caused by COVID-19</Typography>
 					</CardContent>
