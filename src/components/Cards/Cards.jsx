52b73829713a5e6e6050bd7da7fc73cf74b8d000
Chris Patrick <atomize101@gmail.com>
2020-11-02 11:21:56 -0500

    Styling to get colors for cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 
@@ -11,7 +12,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 	return (
 		<div className={styles.container}>
 			<Grid container spacing={3} justify="center">
-				<Grid item component={Card}>
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
 					<CardContent>
 						<Typography color="textSecondary" gutterBottom>
 							Infected
@@ -23,7 +24,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 						<Typography variant="body2">Number of active cases of COVID-19</Typography>
 					</CardContent>
 				</Grid>
-				<Grid item component={Card}>
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
 					<CardContent>
 						<Typography color="textSecondary" gutterBottom>
 							Recovered
@@ -35,13 +36,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 						<Typography variant="body2">Number of recoveries from COVID-19</Typography>
 					</CardContent>
 				</Grid>
-				<Grid item component={Card}>
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
 					<CardContent>
 						<Typography color="textSecondary" gutterBottom>
 							Deaths
 						</Typography>
 						<Typography variant="h5">
-							<CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+							<CountUp start={0} end={deaths.value} duration={2.5} separator="," />
 						</Typography>
 						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
 						<Typography variant="body2">Number of deaths caused by COVID-19</Typography>
