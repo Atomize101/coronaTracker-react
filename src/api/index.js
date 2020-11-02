5ae41406b0162c904ca0ff6bff05e15137de1eee
Chris Patrick <atomize101@gmail.com>
2020-11-02 12:59:23 -0500

    Using a diff api link for daily data

--- src/api/index.js
+++ src/api/index.js
@@ -23,3 +23,18 @@ export const fetchData = async () => {
 		};
 	} catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+	try {
+		const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+
+		return data.map(({ positive, recovered, death, dateChecked: date }) => ({
+			confirmed: positive,
+			recovered,
+			deaths: death,
+			date,
+		}));
+	} catch (error) {
+		return error;
+	}
+};
